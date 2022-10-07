dfb36d6a03858bef0875e0ec13cef0b288aec086
Alexander Yordanov <alex05.yordanov@gmail.com>
2022-10-07 22:47:29 +0300

    Auto zoom meta tag

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
         <title>HOME</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
         <link rel="stylesheet" href="general/headers.css">
-        <meta name="theme-color" content="#76B947" />
+        <meta name="theme-color" content="#76B947"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
     
     <body>
