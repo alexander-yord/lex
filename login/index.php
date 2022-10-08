65274b88f0ed2996db473b6f7604d5ada6edeadc
Alexander Yordanov <alex05.yordanov@gmail.com>
2022-10-08 11:28:28 +0300

    Updated meta tags on all main pages

--- login/index.php
+++ login/index.php
@@ -44,6 +44,9 @@
         </title>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+        <link rel="stylesheet" href="general/headers.css">
+        <meta name="theme-color" content="#76B947"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
     <body>
         <div class="loginSignup">
