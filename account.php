168b0b38891ee586374df25adcdf4216db87fd3d
Alexander Yordanov <alex05.yordanov@gmail.com>
2022-11-06 00:37:57 +0200

    Auto-click follow button if user is following

--- account.php
+++ account.php
@@ -1,5 +1,63 @@
 <?php
     session_start();
+
+    $path = $_SERVER['DOCUMENT_ROOT']; //account.php?id=
+    $path .= "/general/connectionInfo.php";
+    include($path);
+    $account_id = $_GET["id"]; //id of the account
+
+    $sql = "SELECT a.first_name as first_name, a.last_name as last_name, c.username as username 
+        FROM accounts a LEFT JOIN login_credentials c ON
+        a.account_id = c.account_id
+        WHERE a.account_id = '$account_id'";    
+    $result = mysqli_query($conn, $sql);
+    
+    if (mysqli_num_rows($result) > 0) { //check if a record for this account_number is returned
+        $account_exists = true;
+        $row = mysqli_fetch_assoc($result);
+        $first_name = $row["first_name"];
+        $last_name = $row["last_name"];
+        $username = $row["username"];
+    }
+    else {
+        $account_exists = false;
+    }
+    
+    $path = $_SERVER['DOCUMENT_ROOT']; //account.php?id=
+    $path .= "/general/connectionInfo.php";
+    include($path);
+    $account_id = $_GET["id"]; //id of the account
+
+    $sql = "SELECT a.first_name as first_name, a.last_name as last_name, c.username as username 
+            FROM accounts a LEFT JOIN login_credentials c ON
+            a.account_id = c.account_id
+            WHERE a.account_id = '$account_id'";
+    
+    $result = mysqli_query($conn, $sql);
+    
+    if (mysqli_num_rows($result) > 0) { //check if a record for this account_number is returned
+        $account_exists = true;
+        $row = mysqli_fetch_assoc($result);
+        $first_name = $row["first_name"];
+        $last_name = $row["last_name"];
+        $username = $row["username"];
+        }
+    else {
+        $account_exists = false;
+    }
+
+    //check if the user is following this account
+    $following = false;
+    if (isset($_SESSION["id"]) && $account_exists) {
+        $user_account_id = $_SESSION["id"]; //id of the viewer of the account
+        $sql_following = "SELECT uid FROM followers where account_id = '$account_id' 
+                          and follower_id = '$user_account_id'";
+        $result_f = mysqli_query($conn, $sql_following);
+        if (mysqli_num_rows($result_f) > 0) {
+            $following = 1; // evaluates to true in js 
+        }
+    }
+    
 ?>
 
 <!DOCTYPE html>
@@ -11,10 +69,6 @@
         <link rel="stylesheet" href="/general/headers.css">
         <meta name="theme-color" content="#76B947"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-
-</style>
-
     </head>
     
     <body>
@@ -25,37 +79,13 @@
                 <a href="/new">Lex Now!</a>
             </div>
         </div> <br><br>
-        <?php
-            $path = $_SERVER['DOCUMENT_ROOT']; //account.php?id=
-            $path .= "/general/connectionInfo.php";
-            include($path);
-            $account_id = $_GET["id"]; //id of the account
-            $user_account_id = $_SESSION["id"]; //id of the viewer of the account
-
-            $sql = "SELECT a.first_name as first_name, a.last_name as last_name, c.username as username 
-                FROM accounts a LEFT JOIN login_credentials c ON
-                a.account_id = c.account_id
-                WHERE a.account_id = '$account_id'";
-            
-            $result = mysqli_query($conn, $sql);
-    
-            if (mysqli_num_rows($result) > 0) { //check if a record for this account_number is returned
-                $account_exists = true;
-                $row = mysqli_fetch_assoc($result);
-                $first_name = $row["first_name"];
-                $last_name = $row["last_name"];
-                $username = $row["username"];
-            }
-            else {
-                $account_exists = false;
-            }
-        ?>
 
         <?php if($account_exists):?>
             <h2><?php echo $first_name . " " . $last_name; ?></h2> 
             <a>@<?php echo $username; ?></a>
-            <input type="button" value="Follow" id="follow-unfollow" style="float: right;"/>
+            <input type="button" value="Follow!" id="follow-unfollow" style="float: right;"/>
             <br><br>
+
         <?php else: ?>
             <center>
                 <h2>Sorry, this account does not exist!</h2>
@@ -65,9 +95,9 @@
         <script>
             const btn = document.getElementById("follow-unfollow");
             btn.addEventListener("click", ()=>{
-                if(btn.value === "Follow!"){
-                    btn.value = "Unfollow";
-                    btn.style.backgroundColor = "#d0cfcf";
+                if(btn.value === "Follow!"){ // if not following 
+                    btn.value = "Unfollow"; // changes the text to be following 
+                    btn.style.backgroundColor = "#d0cfcf"; 
                     btn.style.color = "#000";
                 }else{
                     if (confirm('Are you sure you want to unfollow this account?')) {
@@ -76,10 +106,13 @@
                         btn.style.color = "white";
                     } else {
                         // Do nothing!
-                        console.log('Thing was not saved to the database.');
                     }
                 }
-            })
+            });
+            
+            if(<?php echo $following ?> == true) { //clicks the following button if the user is following this account
+                document.getElementById("follow-unfollow").click();
+            }
         </script>
         
     </body>
