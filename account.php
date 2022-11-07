3c76895836adb7a2cabcaf743cc95ebf3912f053
Alexander Yordanov <alex05.yordanov@gmail.com>
2022-11-07 18:52:55 +0200

    Debugging followers tab

--- account.php
+++ account.php
@@ -103,35 +103,35 @@ if (isset($_SESSION["id"]) && $account_exists) {
                     echo "<embed type='text/html' src='lex.php?uid=$uid' style='width:100%;height:30vh;'>";
                 }
             } else {
-                echo "<h3>" . $first_name . " has not lexed yet</h3>";
+                echo "<h3>" . $first_name . " has not lexed yet&#1F61E</h3>";
             }
             ?>
         </div>
 
         <div id="followers" class="tabcontent">
-            <!-- ?php
+            <?php
             $sql_followers = "SELECT f.account_id as account_id, 
                               a.first_name as first_name, 
                               a.last_name as last_name, 
                               c.username as username FROM followers f 
                               left join accounts a on f.account_id = a.account_id 
                               left join login_credentials c on f.account_id = c.account_id 
-                              where f.follower_id = $account_id"; //people that this account follows
+                              where f.follower_id = '$account_id'"; //people that this account follows
             $result_followers = mysqli_query($conn, $sql_followers);
             if (mysqli_num_rows($result_followers) > 0) {
                 echo "<h3>" . $first_name . "'s followers:</h3>";
                 echo "<table>";
                 while ($row = mysqli_fetch_assoc($result_followers)) {
                     $f_id = $row["account_id"];
-                    $f_name = $row["first_name"] . " " . $row["last_name"];
+                    $f_name = $row["first_name"] . " " . $row["last_name"]; //folower's name
                     $f_username = $row["username"];
                     echo "<tr><td>$f_name</td><td><a href='/account.php?id=$f_id'>@$f_username</a></td></tr>";
                 }
                 echo "</table>";
             } else {
-                echo "<h3>" . $first_name . " has no followers yet</h3>";
+                echo "<h3>" . $first_name . " has no followers yet&#1F61E</h3>";
             }
-            ?> -->
+            ?>
         </div>
 
         <div id="following" class="tabcontent">
